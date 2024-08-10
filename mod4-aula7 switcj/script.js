794f7603eb2e23368cdcea4a0a0da2c6c3dab61d
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-09 23:07:53 -0300

    switch case

---
+++ mod4-aula7 switcj/script.js
@@ -0,0 +1,12 @@
+let option = 1
+
+switch(option){
+    case 1: 
+        console.log("Consultar pedido");
+        break
+    case 2:
+        console.log("Falar com atendente");
+        break;
+    default:
+        console("Opção inválida");    
+}
