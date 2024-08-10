a3f058764476cf16971d1cd2b19fced0d2c33325
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-09 23:15:00 -0300

    excecoes try-catch-finaly

---
+++ mod4-aula8 excecoes/script.js
@@ -0,0 +1,8 @@
+try{
+    //aqui iremos incluir todo código que iremos rodar e que pode ter erros
+}catch(E){
+    //aqui iremos capturar e tratar esses erros
+} finally{
+    //aqui o código é executado mesmo se o erro for tratado ou não 
+    //ex: fechar conexão com banco de dados, leitos de documentos etc...
+}
