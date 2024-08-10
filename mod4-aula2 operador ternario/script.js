2a869ca59148cfe18d6ea772e3cbf57bd6657267
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-09 22:42:46 -0300

    operador ternario

---
+++ mod4-aula2 operador ternario/script.js
@@ -0,0 +1,9 @@
+let age = 16;
+
+console.log(
+    //testando se idade é maior ou igual a 18
+    age >= 18 
+    //ser for verdadeiro ele vai pode dirigir
+    ? "Você pode dirigir" 
+    //se for falso ele não podeer[a dirigir
+    : "Você não pode dirigir"); 
