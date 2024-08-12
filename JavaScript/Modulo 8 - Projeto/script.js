efeafedf80bbe596e759155d26648c09395ec28e
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-12 16:37:05 -0300

    iniciando o projeto e criando manipulador de input

---
+++ JavaScript/Modulo 8 - Projeto/script.js
@@ -0,0 +1,6 @@
+const amount = document.getElementById("amount");
+
+//manipulando o input amount para receber somente números
+amount.addEventListener("input", () =>{
+    console.log(input.value);
+})
