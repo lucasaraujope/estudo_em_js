97cabc6f1e03a7f6fb93e69d034e21ad86b3bbf3
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-11 22:08:02 -0300

    manipulando elementos

---
+++ JavaScript/Modulo 6 - DOM/aula 8 manipulando atributos/scripts.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("input");
+
+//desabilitando a funcionalide do input
+//input.setAttribute("disabled", true);
+
+//alterando o tipo do input
+//input.setAttribute("type", "file");
+
+//removendo um atribuo do input
+//input.removeAttribute("id")
