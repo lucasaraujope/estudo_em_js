58579179cc739dd0a452de0a0b205d7f4b382d36
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-12 16:50:57 -0300

    recuperando a moeda que o usuário escolheu

--- JavaScript/Modulo 8 - Projeto/script.js
+++ JavaScript/Modulo 8 - Projeto/script.js
@@ -1,4 +1,6 @@
+const form = document.querySelector("forma");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 //manipulando o input amount para receber somente números
 amount.addEventListener("input", () =>{
@@ -9,3 +11,10 @@ amount.addEventListener("input", () =>{
     //atribuindo ao amount apenas valores númericos, pois estamso substituindo todos os caracteres recbidos pelo regez e ignorando eles
     amount.value = amount.value.replace(hasCactersRegex, "");
 })
+
+//capturando o valor que é gerado nas opções de moeda quando clicamos no botão do botão
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    console.log(currency.value);
+}
