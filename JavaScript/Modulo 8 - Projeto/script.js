25bd2d4c012c9499b0b156f00daf2c587ec603d6
Lucas Araujo Pv <110195985+lucasaraupe@users.noreply.github.com>
2024-08-12 16:44:42 -0300

    criando váriais que ignoram caracteres no input

--- JavaScript/Modulo 8 - Projeto/script.js
+++ JavaScript/Modulo 8 - Projeto/script.js
@@ -2,5 +2,10 @@ const amount = document.getElementById("amount");
 
 //manipulando o input amount para receber somente números
 amount.addEventListener("input", () =>{
-    console.log(input.value);
+
+    //cirando variácel que captura todos os caracteres digitados no inpur
+    const hasCactersRegex = /\D+/g;
+
+    //atribuindo ao amount apenas valores númericos, pois estamso substituindo todos os caracteres recbidos pelo regez e ignorando eles
+    amount.value = amount.value.replace(hasCactersRegex, "");
 })
